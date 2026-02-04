b0099abaeb1807d6d161a913d8f5fe01101066ba
GitHub Action <action@github.com>
2026-02-04 14:42:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuyu1815/freevscodeicon/MyBundle.kt
+++ src/main/kotlin/com/github/yuyu1815/freevscodeicon/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.yuyu1815.freevscodeicon
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/yuyu1815/freevscodeicon/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yuyu1815/freevscodeicon/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yuyu1815.freevscodeicon.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
